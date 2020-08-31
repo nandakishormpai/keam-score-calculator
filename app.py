e56e2f9cf0a7b465fa710435a7c61a81266b6d79
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-08-31 11:47:05 +0530

    configed

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask,flash,render_template,request
 from boto.s3.connection import S3Connection
+import os
 app=Flask(__name__)
 
-app.config['SECRET_KEY']=os.environ['SECRET_KEY']
+app.config['SECRET_KEY']=S3Connection(os.environ['SECRET_KEY'])
 
 @app.route('/', methods=['GET', 'POST'])
 def new():
